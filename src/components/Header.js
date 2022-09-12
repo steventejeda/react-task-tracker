dfc027ad9f8bda36473580994cd2ea9965e351e0
Computer <computer@Computers-MacBook-Pro.local>
2022-09-11 23:13:55 -0400

    added some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
-const header = (props) => {
+import PropTypes from 'prop-types';
+
+const header = ({ title }) => {
   return (
     <header>
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
     </header>
   )
 }
+
+header.defaultProps = {
+    title: 'Task Tracker'
+}  
+
+header.propTypes = {
+    title: PropTypes.string.isRequired,
+}   
+
+//CSS in JS
+// const headingStyle = {
+//    color: 'red', backgroundColor: 'black'
+// }
 
 export default header
